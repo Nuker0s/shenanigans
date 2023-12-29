6aa16baa2ce499199ec6edc933c02319e45f120f
Nuker0s <nuker0s2@gmail.com>
2023-12-29 15:35:59 +0100

    antenna update

--- Assets/Scripts/Antenna.cs
+++ Assets/Scripts/Antenna.cs
@@ -9,31 +9,28 @@ public class Antenna : MonoBehaviour
     public SplineContainer sc;
     public Transform tip; //tip of the antenna (must NOT use base as a parent)
     public Transform tiptarget; //where tip of the antenna should be when stationary
-    public float lerpvalue = 30;
+    public Rigidbody tiprb;
+    public float force = 100f;
+    public float verticalspeedmult = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        tip.position = Vector3.Lerp(tip.position, tiptarget.position, lerpvalue * Time.deltaTime);
-        SplineContainer MySpline = sc;
+        var endknot = sc.Spline.ToArray()[1];
+        endknot.Position = sc.transform.InverseTransformPoint(tip.position);
+        sc.Spline.SetKnot(1, endknot);
 
-
-        var firstKnot = MySpline.Spline.ToArray()[1];
-
-        firstKnot.Position = MySpline.transform.InverseTransformPoint(tip.position);
-        //firstKnot.Rotation = Quaternion.Inverse(MySpline.transform.rotation) * KnotTarget.rotation;
-
-
-        MySpline.Spline.SetKnot(1, firstKnot);
-        tip.rotation = firstKnot.Rotation;
-        
-
+    }
+    private void FixedUpdate()
+    {
+        Vector3 dir = tiptarget.position - tip.position;
+        tiprb.AddForce(new Vector3(dir.x, dir.y * verticalspeedmult, dir.z) * force);
     }
 
 }
